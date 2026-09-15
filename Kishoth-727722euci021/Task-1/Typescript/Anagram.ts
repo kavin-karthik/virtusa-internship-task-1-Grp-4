f3a2df181d601008ecb5e49165d5b244376de65d
KishothThangavel <128055714+KishothThangavel@users.noreply.github.com>
2026-09-15 14:28:37 +0530

    Create Anagram.ts

---
+++ Kishoth-727722euci021/Task-1/Typescript/Anagram.ts
@@ -0,0 +1,26 @@
+declare var require: any;
+declare var process: any;
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter first string: ", (first: string) => {
+
+    rl.question("Enter second string: ", (second: string) => {
+
+        let word1 = first.toLowerCase().split("").sort().join("");
+        let word2 = second.toLowerCase().split("").sort().join("");
+
+        if (word1 == word2) {
+            console.log("Anagram");
+        } else {
+            console.log("Not Anagram");
+        }
+
+        rl.close();
+    });
+});
